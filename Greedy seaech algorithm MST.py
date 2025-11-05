abdda9f4d87e6d3c472ab02e0b69741a9c66a590
Nagesh Ige <nageshige@gmail.com>
2025-11-05 19:16:58 +0530

    Add Kruskal's algorithm implementation for MST
    Implement Kruskal's algorithm for finding the Minimum Spanning Tree (MST) using a Union-Find data structure.

---
+++ Greedy seaech algorithm MST.py
@@ -0,0 +1,46 @@
+class UnionFind:
+    def __init__(self, nodes):
+        self.parent = {n: n for n in nodes}
+        self.rank = {n: 0 for n in nodes}
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    def union(self, x, y):
+        rx, ry = self.find(x), self.find(y)
+        if rx == ry:
+            return False
+        if self.rank[rx] < self.rank[ry]:
+            self.parent[rx] = ry
+        elif self.rank[rx] > self.rank[ry]:
+            self.parent[ry] = rx
+        else:
+            self.parent[ry] = rx
+            self.rank[rx] += 1
+        return True
+
+def kruskal(edges):
+    nodes = set()
+    for u, v, w in edges:
+        nodes.add(u); nodes.add(v)
+    uf = UnionFind(nodes)
+    mst = []
+    total = 0
+    for u, v, w in sorted(edges, key=lambda x: x[2]):
+        if uf.union(u, v):
+            mst.append((u, v, w))
+            total += w
+    return mst, total
+
+if __name__ == "__main__":
+    edges = [
+        ('A','B',4), ('A','H',8), ('B','H',11), ('B','C',8),
+        ('C','D',7), ('C','F',4), ('C','I',2), ('D','E',9),
+        ('D','F',14), ('E','F',10), ('F','G',2), ('G','H',1),
+        ('G','I',6), ('H','I',7)
+    ]
+    mst, cost = kruskal(edges)
+    print("MST edges:")
+    for u, v, w in mst:
+        print(u, "-", v, ":", w)
+    print("Total cost:", cost)
